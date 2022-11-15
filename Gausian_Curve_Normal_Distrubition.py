a04cde07fce24e6b3a5bcb64eaa7c70ca7c42f9c
drParadox312 <44335637+drParadox312@users.noreply.github.com>
2022-11-15 23:44:56 +0300

    Update Gausian_Curve_Normal_Distrubition.py

--- Gausian_Curve_Normal_Distrubition.py
+++ Gausian_Curve_Normal_Distrubition.py
@@ -2,26 +2,20 @@ import matplotlib.pyplot as plot
 import random
 
 
-skewness = 0 # change value in range of "-1.0 - 1.0" to see effect
-kurtosis = 1 # change value in range of "0.0 - positive infinity" to see effect
+skewness = 0.0 # change value in range of "-1.0 - 1.0" to see effect
+kurtosis = 1.0 # change value in range of "0.0 - positive infinity" to see effect
 
 
 x_axis_length = 100
-x_axis_min = 0
-x_axis_max = 100
+x_axis_min = 0.0
+x_axis_max = 100.0
 
 
-# t ranges between 0.0 - 1.0
-# values are returned between 0.0 - 1.0
-def get_normalized_unmodified_gausian_value(t:float) -> float:
-    v = 1.0 - (1.0 - ((float(abs(0.5 - t)) - 0.5)/0.5)**2)**2
-    return v
 
 # t ranges between 0.0 - 1.0
 # skewness ranges between -1.0 - 1.0
-# kurtosis ranges between 0.0 - positive infinity
-# values are returned between 0.0 - 1.0
-def get_normalized_modified_gausian_value(t:float, skewness:float, kurtosis:float) -> float:
+# krutosis ranges between 0.0 - positive infinity
+def get_normalized_gausian_value(t:float, skewness=0.0, kurtosis=1.0) -> float:
     skewness = min(1.0, max(-1.0, skewness))
     t = min(1.0, max(0.0, t - skewness * 0.5))
     v = 1.0 - (1.0 - ((float(abs(0.5 - t)) - 0.5)/0.5)**2)**2
@@ -30,15 +24,10 @@ def get_normalized_modified_gausian_value(t:float, skewness:float, kurtosis:floa
     return v
 
 
-# values are returned between 0.0 - 1.0
-def get_normalized_unmodified_gausian_random() -> float:
+def get_normalized_gausian_random(skewness=0.0, kurtosis=1.0) -> float:
     t = random.uniform(0.0, 1.0)
-    return get_normalized_unmodified_gausian_value(t)
+    return get_normalized_gausian_value(t, skewness, kurtosis)
 
-# values are returned between 0.0 - 1.0
-def get_normalized_modified_gausian_random(skewness:float, kurtosis:float) -> float:
-    t = random.uniform(0.0, 1.0)
-    return get_normalized_modified_gausian_value(t, skewness, kurtosis)
 
 
 def draw_graph() -> None:
@@ -49,7 +38,8 @@ def draw_graph() -> None:
         t = float(i) / float(x_axis_length)
         x = x_axis_min + x_axis_range * t
         x_axis.append(x)
-        y = get_normalized_modified_gausian_value(t, skewness, kurtosis)
+        y = get_normalized_gausian_value(t, skewness, kurtosis)
+        #y = get_normalized_gausian_random(skewness, kurtosis)
         y_axis.append(y)
     plot.plot(x_axis, y_axis, color="g")
     plot.show()
